3a5fd19c04f420d09855e25d70b4beb2f607eb54
Cihan Çoban <30843056+MaximillianFoe@users.noreply.github.com>
2021-01-15 12:01:45 +0300

    I think program going to work better with JSoup. Apache is using more sources and more complicated. I changed to JSoup and also i'll update code in next hours, it's now giving some SSL errors for no reason.

--- DownloadSystem/DownloadURL.java
+++ DownloadSystem/DownloadURL.java
@@ -1,2 +1,17 @@
+import org.jsoup.Connection;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import java.io.IOException;
+
 public class DownloadURL {
+    public static void main(String[] args) throws IOException {
+        String page = "https://ce.ieu.edu.tr/tr/syllabus/type/read/id/SE+302/";
+        Connection conn = Jsoup.connect(page);
+        Document doc = conn.get();
+        String courseName = doc.body().text();
+        String courseCode = doc.body().text();
+        String termDate = doc.body().text();
+        String result = doc.body().text();
+        System.out.println(result);
+}
 }
