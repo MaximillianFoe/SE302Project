34d1a001d045c0d577625a2d8bdb7865d4ada673
Cihan Çoban <30843056+MaximillianFoe@users.noreply.github.com>
2021-01-15 12:33:42 +0300

    Updated variable names. Also fixed SSL problem with importing SSL certificate, working well. Now to divide parts and write it to a file.

--- DownloadSystem/DownloadURL.java
+++ DownloadSystem/DownloadURL.java
@@ -5,13 +5,13 @@ import java.io.IOException;
 
 public class DownloadURL {
     public static void main(String[] args) throws IOException {
-        String page = "https://ce.ieu.edu.tr/tr/syllabus/type/read/id/SE+302/";
-        Connection conn = Jsoup.connect(page);
-        Document doc = conn.get();
-        String courseName = doc.body().text();
-        String courseCode = doc.body().text();
-        String termDate = doc.body().text();
-        String result = doc.body().text();
+        String courseSite = "https://ce.ieu.edu.tr/tr/syllabus/type/read/id/SE+302";
+        Connection connectionToCourse = Jsoup.connect(courseSite);
+        Document sitePart = connectionToCourse.get();
+        String courseName = sitePart.body().text();
+        String courseCode = sitePart.body().text();
+        String termDate = sitePart.body().text();
+        String result = sitePart.body().text();
         System.out.println(result);
 }
 }
