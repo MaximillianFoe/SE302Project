d6fc10b5f6a5ff9c3e28155d2a2725a0baaa58a2
Cihan Çoban <30843056+MaximillianFoe@users.noreply.github.com>
2021-01-15 13:03:58 +0300

    It's working like a charm! getElementById is configured; courseCode and Faculty sections are imported to code. Now i'll create all wanted data structure from sample syllabus.

--- DownloadSystem/DownloadURL.java
+++ DownloadSystem/DownloadURL.java
@@ -2,16 +2,19 @@ import org.jsoup.Connection;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import java.io.IOException;
+import java.util.Scanner;
+import org.jsoup.select.Elements;
 
 public class DownloadURL {
     public static void main(String[] args) throws IOException {
-        String courseSite = "https://ce.ieu.edu.tr/tr/syllabus/type/read/id/SE+302";
-        Connection connectionToCourse = Jsoup.connect(courseSite);
-        Document sitePart = connectionToCourse.get();
-        String courseName = sitePart.body().text();
-        String courseCode = sitePart.body().text();
-        String termDate = sitePart.body().text();
-        String result = sitePart.body().text();
-        System.out.println(result);
+        String courseCode = "SE+302";
+        String Faculty = "ce";
+        String courseSite = "https://" + Faculty + ".ieu.edu.tr/tr/syllabus/type/read/id/" + courseCode; // Sitenin adresi belirli; facultyName ve courseCode kısmını kullanıcı belirliyor.
+        Connection connectionToCourse = Jsoup.connect(courseSite); // Sayfaya bağlanmam için JSoup komutu.
+        Document sitePart = connectionToCourse.get(); // Siteden verileri çekiyoruz.
+
+        String courseName = sitePart.getElementById("course_name").text(); // ID kısmını sayfa kaynağını inceleyerek alıyoruz.
+        String termDate = sitePart.getElementById("semester").text();
+        System.out.println(courseName + termDate);
 }
 }
