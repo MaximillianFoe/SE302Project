e90e021aced489bd88419eef4f0f77f072d0fb6a
Cihan Çoban <30843056+MaximillianFoe@users.noreply.github.com>
2021-01-16 15:37:27 +0300

    Data structure is on progress.

--- DownloadSystem/DownloadURL.java
+++ DownloadSystem/DownloadURL.java
@@ -2,8 +2,6 @@ import org.jsoup.Connection;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import java.io.IOException;
-import java.util.Scanner;
-import org.jsoup.select.Elements;
 
 public class DownloadURL {
     public static void main(String[] args) throws IOException {
@@ -15,6 +13,17 @@ public class DownloadURL {
 
         String courseName = sitePart.getElementById("course_name").text(); // ID kısmını sayfa kaynağını inceleyerek alıyoruz.
         String termDate = sitePart.getElementById("semester").text();
+        String theoryTime = sitePart.getElementById("weekly_hours").text();
+        String labTime = sitePart.getElementById("app_hours").text();
+        String localCredits = sitePart.getElementById("ieu_credit").text();
+        String ectsCredits = sitePart.getElementById("ects_credit").text();
+        String preRequisites = sitePart.getElementById("pre_requisites").text();
+        String courseLanguage = sitePart.getElementById("course_lang").text();
+        String courseType = sitePart.getElementById("course_type").text();
+        String courseLevel = sitePart.getElementById("course_level").text();
+        String courseCoordinator = sitePart.getElementById("coordinator_list").text();
+        String courseLecturer = sitePart.getElementById("lecturer_list").text();
+
         System.out.println(courseName + termDate);
 }
 }
