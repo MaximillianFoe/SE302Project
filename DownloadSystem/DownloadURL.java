8bee20e1f77245319e1a29ca3722aa97a1a82136
Cihan Çoban <30843056+MaximillianFoe@users.noreply.github.com>
2021-01-17 14:01:34 +0300

    Hour Number and Duration structure is done!

--- DownloadSystem/DownloadURL.java
+++ DownloadSystem/DownloadURL.java
@@ -51,8 +51,11 @@ public class DownloadURL {
         String presentNumber = sitePart.getElementById("presentation_no").text();
         String presentPer = sitePart.getElementById("presentation_per").text();
 
-        String projectnumber = sitePart.getElementById("project_no").text();
+        String projectNumber = sitePart.getElementById("project_no").text();
         String projectPer = sitePart.getElementById("project_per").text();
+
+        String seminarNumber = sitePart.getElementById("seminar_no").text();
+        String seminarPer = sitePart.getElementById("seminar_per").text();
 
         String portNumber = sitePart.getElementById("portfolios__no").text();
         String portPer = sitePart.getElementById("portfolios_per").text();
@@ -66,11 +69,68 @@ public class DownloadURL {
         String totalNumber = sitePart.getElementById("ara_total_no").text();
         String totalPer = sitePart.getElementById("ara_total_per").text();
 
-        // AKTS tablosu için veri yapısı oluşturuyoruz.
-        // Normalde 12x3 olması gerekiyor ancak son tabloyu çarpım olarak yapacağım.
+        // AKTS tablosu için veri yapısı oluşturuyoruz. aşağı yukarı 11x3 bir tablo olması gerekiyor.
 
+        String courseHourNumber = sitePart.getElementById("course_hour_number").text();
+        String courseHourDuration = sitePart.getElementById("course_hour_duration").text();
+        String courseHourWLoad = sitePart.getElementById("course_hour_total_workload").text();
 
+        String labHourNumber = sitePart.getElementById("lab_number").text();
+        String labHourDuration = sitePart.getElementById("lab_duration").text();
+        String labHourWLoad = sitePart.getElementById("lab_total_workload").text();
 
+        String outHourNumber = sitePart.getElementById("out_hour_number").text();
+        String outHourDuration = sitePart.getElementById("out_hour_duration").text();
+        String outHourWLoad = sitePart.getElementById("out_hour_total_workload").text();
+
+        String fieldworkHourNumber = sitePart.getElementById("fieldwork_number").text();
+        String fieldworkHourDuration = sitePart.getElementById("fieldwork_duration").text();
+        String fieldworkHourWLoad = sitePart.getElementById("fieldwork_total_number").text();
+
+        String quizHourNumber = sitePart.getElementById("quizess_number").text();
+        String quizHourDuration = sitePart.getElementById("quizess_duration").text();
+        String quizHourWLoad = sitePart.getElementById("quizess_total_workload").text();
+
+        String hWorkHourNumber = sitePart.getElementById("homework_number").text();
+        String hWorkHourDuration = sitePart.getElementById("homework_duration").text();
+        String hWorkHourWLoad = sitePart.getElementById("homework_total_workload").text();
+
+        String presentHourNumber = sitePart.getElementById("presentation_number").text();
+        String presentHourDuration = sitePart.getElementById("presentation_duration").text();
+        String presentHourWLoad = sitePart.getElementById("presentation_total_workload").text();
+
+        String projectHourNumber = sitePart.getElementById("project_number").text();
+        String projectHourDuration = sitePart.getElementById("project_duration").text();
+        String projectHourWLoad = sitePart.getElementById("project_total_workload").text();
+
+        String seminarHourNumber = sitePart.getElementById("seminar_number").text();
+        String seminarHourDuration = sitePart.getElementById("seminar_duration").text();
+        String seminarHourWLoad = sitePart.getElementById("seminar_total_workload").text();
+
+        String portHourNumber = sitePart.getElementById("portfolios_number").text();
+        String portHourDuration = sitePart.getElementById("portfolios_duration").text();
+        String portHourWLoad = sitePart.getElementById("portfolios_total_workload").text();
+
+        String midtermHourNumber = sitePart.getElementById("midterm_number").text();
+        String midtermHourDuration = sitePart.getElementById("midterm_duration").text();
+        String midtermHourWLoad = sitePart.getElementById("midterm_total_workload").text();
+
+        String finalHourNumber = sitePart.getElementById("final_number").text();
+        String finalHourDuration = sitePart.getElementById("final_duration").text();
+        String finalHourWLoad = sitePart.getElementById("final_total_workload").text();
+
+        // Toplam yükü belirlemek için bütün yapıyı integer'e çevirmemiz gerekiyor.
+        int iCH=Integer.parseInt(courseHourWLoad);
+        int iLH=Integer.parseInt(labHourWLoad);
+        int iOH=Integer.parseInt(outHourWLoad);
+        int iFH=Integer.parseInt(fieldworkHourWLoad);
+        int iQH=Integer.parseInt(quizHourWLoad);
+        int iHH=Integer.parseInt(hWorkHourWLoad);
+        int iPH=Integer.parseInt(presentHourWLoad);
+        int iPJH=Integer.parseInt(projectHourWLoad);
+        int iPOH=Integer.parseInt(portHourWLoad);
+        int iMH=Integer.parseInt(midtermHourWLoad);
+        int iFH=Integer.parseInt(finalHourWLoad);
 
 
     }
