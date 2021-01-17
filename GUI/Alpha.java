235d210720e8ded5fa9bbf7f8e1c16067726fb99
Cihan Çoban <30843056+MaximillianFoe@users.noreply.github.com>
2021-01-17 14:54:54 +0300

    MainFrame class is created. Also POI library is added for Excel creation. Also GUI is revisited with proper heights.

--- GUI/Alpha.java
+++ GUI/Alpha.java
@@ -8,24 +8,28 @@ public class Alpha {
     public static void main(String[] args) {
         JFrame Syllabus = new JFrame(); // Ana pencereyi oluşturuyoruz.
         JButton Download = new JButton("Download Course"); // Download butonunu oluşturuyoruz.
-        Syllabus.setSize(300,300); // Ana pencerenin boyutlarını belirliyoruz.
-        Download.setBounds(130,100,133, 35); // Download butonu için koordinatlar.
+        JButton CourseDatabase = new JButton("Downloaded List"); // İndirilenleri görüntülemek için buton oluşturuyoruz.
+        Syllabus.setSize(300,409); // Ana pencerenin boyutlarını belirliyoruz.
+        Download.setBounds(130,215,133, 35); // Download butonu için koordinatlar.
+        CourseDatabase.setBounds(130,258,133, 35); // Download butonu için koordinatlar.
         Syllabus.setLayout(null); // Layout ayarlarını şimdilik boş bıraktık.
 
         JLabel FacultyListText = new JLabel("Select Faculty:"); //Ekrandaki yerini sonra belirleyeceğiz.
         String[] Faculty = { "CE","BME","EEE","IE","DFE","GBE","AE","CIE","ME","MCE","SE"};
         JList FacultyList = new JList(Faculty); //Listeyi oluşturuyoruz.
         FacultyList.setSelectedIndex(0); // Default olarak seçili değeri ayarlıyoruz, ben CE ayarladım.
-        FacultyList.setBounds(50,50,70,188); // Listenin uzunluğunu test ederek gerekli şekilde ayarladık.
+        FacultyList.setBounds(50,165,70,188); // Listenin uzunluğunu test ederek gerekli şekilde ayarladık.
 
         JTextField CourseCode = new JTextField("Course Code"); // Ders kodunu gireceğimiz alanı oluşturduk.
         CourseCode.setEditable(true); // Düzenlenebilir yaptık.
-        CourseCode.setBounds(130,50,133,35);
-        FacultyListText.setBounds(50,20,133,35); // Uyarı yazısının bölgesini ve boyutunu yazıyoruz.
+        CourseCode.setBounds(130,165,133,35);
+        FacultyListText.setBounds(50,135,133,35); // Uyarı yazısının bölgesini ve boyutunu yazıyoruz.
 
+        // Ekrana öğelerimizi ekliyoruz.
         Syllabus.add(FacultyListText); // Fakülte seçimi için uyarı yazısını ekledik.
         Syllabus.add(FacultyList); // Listeyi pencereye ekliyoruz.
         Syllabus.add(Download); // Butonu oluşturduk ancak şimdi pencereye eklememiz gerek.
+        Syllabus.add(CourseDatabase); // Butonu oluşturduk ancak şimdi pencereye eklememiz gerek.
         Syllabus.add(CourseCode); // Ders kodunu yazacağımız kutuyu ekliyoruz.
 
         Image ieuLogo = null;
