f066e6bf07a55298cd065a08519199519ebeb25c
Cihan Çoban <30843056+MaximillianFoe@users.noreply.github.com>
2021-01-18 21:27:57 +0300

    Language selection is added and working great!

--- MainFrame/Alpha.java
+++ MainFrame/Alpha.java
@@ -10,9 +10,21 @@ public class Alpha {
         JButton Download = new JButton("Download Course"); // Download butonunu oluşturuyoruz.
         JButton CourseDatabase = new JButton("Downloaded List"); // İndirilenleri görüntülemek için buton oluşturuyoruz.
         Syllabus.setSize(300,409); // Ana pencerenin boyutlarını belirliyoruz.
-        Download.setBounds(130,215,133, 35); // Download butonu için koordinatlar.
-        CourseDatabase.setBounds(130,258,133, 35); // İndirilenleri görüntülemek için koordinatlar.
+        Download.setBounds(130,245,133, 35); // Download butonu için koordinatlar.
+        CourseDatabase.setBounds(130,288,133, 35); // İndirilenleri görüntülemek için koordinatlar.
         Syllabus.setLayout(null); // Layout ayarlarını şimdilik boş bıraktık.
+
+
+        JLabel selectionText = new JLabel("Language:"); //Ekrandaki yerini sonra belirleyeceğiz.
+        selectionText.setBounds(150,155,133,35); // Uyarı yazısının bölgesini ve boyutunu yazıyoruz.
+        ButtonGroup languageSelection = new ButtonGroup();
+        JRadioButton turkishLanguage = new JRadioButton("TR", true);
+        turkishLanguage.setBounds(130,175,60,30);
+        JRadioButton englishLanguage = new JRadioButton("EN", false);
+        englishLanguage.setBounds(180,175,60,30);
+        languageSelection.add(turkishLanguage);
+        languageSelection.add(englishLanguage);
+
 
         JLabel FacultyListText = new JLabel("Select Faculty:"); //Ekrandaki yerini sonra belirleyeceğiz.
         FacultyListText.setBounds(45,135,133,35); // Uyarı yazısının bölgesini ve boyutunu yazıyoruz.
@@ -25,14 +37,14 @@ public class Alpha {
 
         JTextField CourseName = new JTextField("SE"); // Ders kodunu gireceğimiz alanı oluşturduk.
         CourseName.setEditable(true); // Düzenlenebilir yaptık.
-        CourseName.setBounds(130,175,55,35);
+        CourseName.setBounds(130,205,55,35);
 
         JLabel PlusSign = new JLabel("+"); //Ekrandaki yerini sonra belirleyeceğiz.
-        PlusSign.setBounds(190,185,10,10); // Uyarı yazısının bölgesini ve boyutunu yazıyoruz.
+        PlusSign.setBounds(190,215,10,10); // Uyarı yazısının bölgesini ve boyutunu yazıyoruz.
 
         JTextField CourseCode = new JTextField("302"); // Ders kodunu gireceğimiz alanı oluşturduk.
         CourseCode.setEditable(true); // Düzenlenebilir yaptık.
-        CourseCode.setBounds(205,175,55,35);
+        CourseCode.setBounds(205,205,55,35);
 
         // Ekrana öğelerimizi ekliyoruz.
         Syllabus.add(FacultyListText); // Fakülte seçimi için uyarı yazısını ekledik.
@@ -42,6 +54,9 @@ public class Alpha {
         Syllabus.add(CourseDatabase); // Butonu oluşturduk ancak şimdi pencereye eklememiz gerek.
         Syllabus.add(CourseCode); // Ders kodunu yazacağımız kutuyu ekliyoruz.
         Syllabus.add(CourseName); // Ders kodunu yazacağımız kutuyu ekliyoruz.
+        Syllabus.add(turkishLanguage); // Türkçe dil seçeneği.
+        Syllabus.add(englishLanguage); // İngilizce dil seçeneği.
+        Syllabus.add(selectionText); // Dil seçimi uyarısı.
 
         Image ieuLogo = null;
         try {
@@ -58,11 +73,18 @@ public class Alpha {
 
         Download.addActionListener(pressDownloadButton -> {
             String selectedFaculty = FacultyList.getSelectedValue();
-            String FirstBox = CourseName.getText();
+            String prefLanguage = "tr"; // Varsayılan değerimiz Türkçe.
+            if(turkishLanguage.isSelected()){
+                prefLanguage = "tr";
+            }
+            if(englishLanguage.isSelected()){
+                prefLanguage = "en";
+            }
+                String FirstBox = CourseName.getText();
             String SecondBox = CourseCode.getText();
             String selectedCourse = FirstBox + "+" + SecondBox; // Okulun sitesindeki format böyle.
             try {
-                DownloadURL.main(selectedFaculty, selectedCourse);
+                DownloadURL.main(selectedFaculty, selectedCourse, prefLanguage);
             } catch (IOException e) {
                 JOptionPane.showMessageDialog(Syllabus, "Something went wrong.");
             }
